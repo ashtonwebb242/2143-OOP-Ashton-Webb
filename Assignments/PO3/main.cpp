792ee10393ddbe646144cbbff062af97a6c780e4
Ashton <89478222+ashtonwebb242@users.noreply.github.com>
2021-11-27 18:12:17 -0600

    Add files via upload

--- Assignments/PO3/main.cpp
+++ Assignments/PO3/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "game.hpp"
 
+using namespace std;
+
+int main()
+{
+    // different results everytime
+    srand(time(0));
+
+    // create a game
+    Game game = Game("input1.txt");
+
+    // start game
+    game.start();
+}
